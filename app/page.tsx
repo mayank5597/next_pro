ae05df007524f75c0cd9da62ab1f6e9020f7482b
mayank.gupta <mayank.gupta@programming.com>
2025-09-26 16:17:43 +0530

    code refacot

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,6 @@ export default function Page() {
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop version"
           />
-          
           <Image
             src="/hero-mobile.png"
             width={560}
